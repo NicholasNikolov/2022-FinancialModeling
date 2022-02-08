1c017d61bf8869415ba78fa3237aee5f6b920600
Nicholas <nikolovnickv@gmail.com>
2022-02-07 22:08:05 -0600

    Zenserp API client created. Sentiment analysis code started
    zenserp will be used to efficiently scrape from google. Might build a method in house using beautiful soup but wanted to experiment with API call.
    
    Created the sentiment analysis file. Will determine sentiment analysis scores through this and supporting methods.

---
+++ run/run_sentiment_analysis.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 21:46:45 2022
+
+@author: nikol
+"""
+
+import nltk
--- src/zenserp.py
+++ src/zenserp.py
@@ -6,9 +6,35 @@ Created on Mon Feb  7 20:35:07 2022
 """
 import zenserp
 
-def establish_zenserp_client():
-    print("src.zenserp.establish_zenserp_client()")
-    '''
-    '''
-    API_KEY = '2d3e1b70-8887-11ec-9541-fdb5a8979f9d'
-    client = zenserp.client(API_KEY)
+class zenserp_client(object):
+    def __init__(self):
+        self.client = self.establish_zenserp_client()
+        
+    def establish_zenserp_client(self):
+        print("src.zenserp.establish_zenserp_client()")
+        '''
+        Establish the API client with the appropriate API key.
+        '''
+        API_KEY = '2d3e1b70-8887-11ec-9541-fdb5a8979f9d'
+        client = zenserp.Client(API_KEY)
+        
+        return client
+    
+    def search_google(self , ticker):
+        print("Running src.zenserp.search_google")
+        '''
+        Perform the zenserp search
+        '''
+        client = self.client
+        
+        params = (
+            ('q', ticker),
+            ('location', 'United States'),
+            ('search_engine', 'google.com'),
+            ('num' , '5'),
+            ('tbm' , 'nws'),
+        )
+        
+        
+        result = client.search(params)
+        return result
